f5c78a81350bea450d7ac3dd8c6293e9ed6354ce
Oleh Tsap <olehtsap07@gmail.com>
2023-08-03 19:03:57 +0300

    не величкі зміни

--- my-first-app/src/components/App.jsx
+++ my-first-app/src/components/App.jsx
@@ -1,14 +1,14 @@
 import painting from '../painting.json'
 import { Container } from './Container/Container';
 import { GalleryList } from "./GalleryList/GalleryList";
-import recepi from '../recepi.json';
+import recipe from '../recipe.json';
 import { Recipe } from './Recipe/Recipe'
  
 
 export const App = () => {
   return (
     <>
-      {recepi.map(({name, image, time, servings, calories}) => {
+      {recipe.map(({name, image, time, servings, calories}) => {
         return (
         <Recipe 
           key={name}
